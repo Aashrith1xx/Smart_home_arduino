64f8433cd0375c7ff507b24975d9069e10bfa571
Aashrith1x <110721406+Aashrith1xx@users.noreply.github.com>
2024-06-01 21:21:23 +0800

    Update fan_pot.ino

--- fan_pot.ino
+++ fan_pot.ino
@@ -1,11 +1,13 @@
-const int potPin = A0;
-const int fanPin = 9;
-void setup {
-pinMode(fanPin, OUTPUT);
+const int potPin = A0;       // Define the analog pin for the potentiometer
+const int fanPin = 9;        // Define the digital pin for the fan
+
+void setup() {
+  pinMode(fanPin, OUTPUT);   // Set the fan pin as an output
 }
-void loop ( {
-int potValue = analogRead (potPin);
-int fanSpeed = map (potValue, 0, 1023,
-0, 255);
-analogWrite(fanPin, fanSpeed);
-delay (10);
+
+void loop() {
+  int potValue = analogRead(potPin);            // Read the potentiometer value
+  int fanSpeed = map(potValue, 0, 1023, 0, 255); // Map the potentiometer value to a range from 0 to 255
+  analogWrite(fanPin, fanSpeed);                // Set the fan speed using PWM
+  delay(10);                                    // Small delay for stability
+}
